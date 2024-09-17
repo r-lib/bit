30032a93d7e2777a47bc174aaa5fcc34acb5b5ec
Jens Oehlschlägel <jens.oehlschlaegel@truecluster.com>
2024-09-17 22:08:19 +0200

    rhub_setup()

--- R/bit-package.R
+++ R/bit-package.R
@@ -19,17 +19,6 @@ NULL
 # devtools::use_vignette("bit-usage")
 # devtools::use_vignette("bit-performance")
 
-# require(rhub)
-# rhub_bit_4.5.0 <- check_for_cran(
-#   path = "../bit_4.5.0.tar.gz"
-# , email = "Jens.Oehlschlaegel@truecluster.com"
-# , check_args = "--as-cran"
-# , env_vars = c('_R_CHECK_FORCE_SUGGESTS_'= "false",'_R_CHECK_CRAN_INCOMING_USE_ASPELL_'= "true", '_R_CHECK_XREFS_MIND_SUSPECT_ANCHORS_'="true")
-# , platforms = NULL
-# , show_status = FALSE
-# )
-
-
 # olddir <- "../revdepold"
 # newdir <- "../revdepnew"
 # tools::check_packages_in_dir(olddir,
--- R/zzz.R
+++ R/zzz.R
@@ -28,3 +28,23 @@
    bit_done()
    library.dynam.unload("bit", libpath)
 }
+
+
+# require(rhub)
+# rhub_bit_4.5.0 <- check_for_cran(
+#   path = "../bit_4.5.0.tar.gz"
+# , email = "Jens.Oehlschlaegel@truecluster.com"
+# , check_args = "--as-cran"
+# , env_vars = c('_R_CHECK_FORCE_SUGGESTS_'= "false",'_R_CHECK_CRAN_INCOMING_USE_ASPELL_'= "true", '_R_CHECK_XREFS_MIND_SUSPECT_ANCHORS_'="true")
+# , platforms = NULL
+# , show_status = FALSE
+# )
+
+# > require(rhub)
+# > rhub_setup()
+# > rhub_doctor()
+# > rhub_check(platforms = c("linux", "macos", "windows", "ubuntu-clang", "ubuntu-gcc12"))
+# ✔ Found git repository at /home/jo/SIK/truecluster/bit.
+# ✔ Found GitHub PAT.                                 
+# ✔ Check started: linux, macos, windows, ubuntu-clang, ubuntu-gcc12 (aspherical-sphinx).
+# See <https://github.com/truecluster/bit/actions> for live output!
