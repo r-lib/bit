e8171ebea3451696e7a23b7900eab9146f7e180a
Michael Chirico <michaelchirico4@gmail.com>
2025-02-10 23:57:09 +0000

    missing 'return'

--- src/attrutil.c
+++ src/attrutil.c
@@ -4,5 +4,5 @@
 SEXP R_bit_set_attr(SEXP x, SEXP which, SEXP value)
 {
   /* xx looking at R sources setAttrib would directly accept a string, however this is not documented */
-  setAttrib(x, install(CHAR(STRING_ELT(which, 0))), value);
+  return setAttrib(x, install(CHAR(STRING_ELT(which, 0))), value);
 }
